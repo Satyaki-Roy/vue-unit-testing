f6a128b708eed59ad026485f35753c32c1e9c4bc
Satyaki Roy <satyaki.roy@morningstar.com>
2020-11-17 11:20:42 +0530

    Mounting components with vue test utils

---
+++ specs/test.spec.js
@@ -0,0 +1,17 @@
+import TestComponent from "@/test";
+import List from "@/list";
+import { mount, shallowMount } from "@vue/test-utils";
+
+test("mount a vue component", () => {
+  const wrapper = mount(TestComponent, {
+    propsData: {
+      value: "VueSchool",
+    },
+  });
+  expect(wrapper.html()).toMatchSnapshot();
+});
+
+test("ListComponent shallow", () => {
+  console.log(mount(List).html());
+  console.log(shallowMount(List).html());
+});
