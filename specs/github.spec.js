ffe6d95ef32b22cb7766973ebefff775091ae6c4
Satyaki Roy <satyaki.roy@morningstar.com>
2020-11-17 16:09:14 +0530

    testing vue component methods

--- specs/github.spec.js
+++ specs/github.spec.js
@@ -1,6 +1,32 @@
-import GithubCard from '@/github-card'
-import { mount } from '@vue/test-utils'
+import GithubCard from "@/github-card";
+import { mount } from "@vue/test-utils";
 
-describe('methods', () => {
+describe("methods", () => {
+  test("composeUrl", () => {
+    const { composeUrl } = GithubCard.methods;
+    expect(composeUrl("123")).toBe("https://api.github.com/users/123");
+    expect(composeUrl("Satyaki-Roy")).toBe(
+      "https://api.github.com/users/Satyaki-Roy"
+    );
+  });
 
-})
+  test("fetchData", async () => {
+    const jsonMock = jest.fn().mockResolvedValue("GITHUB DATA");
+
+    window.fetch = jest.fn().mockResolvedValue({
+      json: jsonMock,
+    });
+
+    const wrapper = mount(GithubCard);
+
+    wrapper.vm.username = "url";
+
+    await wrapper.vm.fetchData();
+
+    expect(window.fetch).toHaveBeenCalledWith(
+      "https://api.github.com/users/url"
+    );
+    expect(jsonMock).toHaveBeenCalled();
+    expect(wrapper.vm.data).toBe("GITHUB DATA");
+  });
+});
