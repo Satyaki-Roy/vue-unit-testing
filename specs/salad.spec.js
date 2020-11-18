9042c820f2b0687702155ba085c9eeb5c4b9f3a7
Satyaki Roy <satyaki.roy@morningstar.com>
2020-11-18 14:07:39 +0530

    vuex testing

--- specs/salad.spec.js
+++ specs/salad.spec.js
@@ -1,6 +1,28 @@
-import Vuex from 'vuex'
-import Vue from 'vue'
-import { mount } from '@vue/test-utils'
+import Vuex from "vuex";
+import { mount, createLocalVue } from "@vue/test-utils";
 
-import SaladBowlComponent from '@/salad-bowl'
-import saladStore from '@/store/salad-store'
+import SaladBowlComponent from "@/salad-bowl";
+import saladStore from "@/store/salad-store";
+
+const VueWithVuex = createLocalVue();
+VueWithVuex.use(Vuex);
+
+test("store is loaded", () => {
+  const store = new Vuex.Store(saladStore);
+  const wrapper = mount(SaladBowlComponent, {
+    store,
+    localVue: VueWithVuex,
+  });
+  store.state.salad.push("mango");
+  expect(wrapper.vm.salad).toEqual(expect.arrayContaining(["mango"]));
+});
+
+test("store works", () => {
+  const store = new Vuex.Store(saladStore);
+  const wrapper = mount(SaladBowlComponent, {
+    store,
+    localVue: VueWithVuex,
+  });
+  wrapper.vm.addIngredient("tomato");
+  expect(wrapper.vm.salad).toEqual(expect.arrayContaining(["tomato"]));
+});
