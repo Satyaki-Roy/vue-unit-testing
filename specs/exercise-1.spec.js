4406fd33417c95d1de7d97b399d3d5de0b0af019
Satyaki Roy <satyaki.roy@morningstar.com>
2020-11-18 15:50:47 +0530

    exercise-1

--- specs/exercise-1.spec.js
+++ specs/exercise-1.spec.js
@@ -1,2 +1,25 @@
-import UserList from '@/exercise-1';
-import { mount } from '@vue/test-utils';
+import { mount } from "@vue/test-utils";
+import { name } from "faker";
+import UserList from "@/exercise-1";
+
+test("component renders the user", () => {
+  const wrapper = mount(UserList, {
+    propsData: {
+      users: [name.findName()],
+    },
+  });
+
+  const li = wrapper.find("li");
+
+  expect(li.text()).toBe(wrapper.props("users")[0]);
+
+  wrapper.setProps({
+    users: [name.findName(), name.findName()],
+  });
+
+  wrapper.vm.$nextTick(() => {
+    const li = wrapper.findAll("li");
+    expect(li.length).toBe(2);
+    expect(li.at(0).text()).toBe(wrapper.props("users")[0]);
+  });
+});
