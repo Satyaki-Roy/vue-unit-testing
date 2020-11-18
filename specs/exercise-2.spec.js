25a4296f7ecd5a6971babeff675413e0bf83da25
Satyaki Roy <satyaki.roy@morningstar.com>
2020-11-18 16:54:01 +0530

    exercise-2

--- specs/exercise-2.spec.js
+++ specs/exercise-2.spec.js
@@ -1,2 +1,23 @@
-import ExerciseForm from '@/exercise-2';
-import { mount } from '@vue/test-utils';
+import ExerciseForm from "@/exercise-2";
+import { createWrapper, mount } from "@vue/test-utils";
+import { nextTick } from "vue";
+
+test("follow the user through the form", async () => {
+  const wrapper = mount(ExerciseForm);
+  expect(wrapper).toMatchSnapshot();
+
+  const form = wrapper.find("form");
+  const input = wrapper.find("input");
+
+  input.setValue("Breakfast");
+  form.trigger("submit");
+
+  await nextTick();
+  expect(wrapper).toMatchSnapshot();
+
+  const button = wrapper.find("li").find("button");
+  button.trigger("click");
+
+  await nextTick();
+  expect(wrapper).toMatchSnapshot();
+});
